0a06640ed1fa5f4f7339a01f088d7984866e9394
Cheeyau <cheeyau@live.com>
2022-11-03 17:40:36 +0100

    Remove connection string

--- widget_co/DAL/ContextString.cs
+++ widget_co/DAL/ContextString.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 
 namespace DAL
 {
+    //return connnection string
     internal class ContextString
     {
-        private static string ConnString { get => "Server=tcp:assignmentcloud.database.windows.net,1433;Initial Catalog=assignmentcloudDB;Persist Security Info=False;User ID=645698_student.inholland.nl#EXT#@CheeyauInholland.onmicrosoft.com;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Authentication=\"Active Directory Integrated\";" +
-                ""; }
+        private static string ConnString { get => "place here your connection string"; }
         protected string getString()
         {
             return ConnString;
